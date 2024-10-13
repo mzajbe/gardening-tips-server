bfa2d7f303b8b69f96e9281dc1fcc839d76afe23
mzajbe <mzajbe@gmail.com>
2024-10-13 20:46:27 +0600

    fix issue cors

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,3 @@
-// import express, { Request, Response } from "express";
-// import cors from 'cors';
-// import globalErrorHandler from "./middlewares/globalErrorhandler";
-// import notFound from "./middlewares/notFound";
-// import router from "./routes";
-// import cookieParser from 'cookie-parser'
-
-
-// const app = express()
-
-
-// //parsers
-// app.use(express.json());
-// app.use(cookieParser())
-// app.use(cors({origin:'http://localhost:5173',credentials:true}));
-
-
-
-// // application routes
-// app.use('/api/v1', router);
-
-// const test = (req: Request, res: Response) => {
-//   res.send('gardening server')
-// };
-
-// app.get('/', test);
-
-
-// app.use(globalErrorHandler);
-
-// //Not Found
-// app.use(notFound);
-
-// export default app;
-
-//  new stats here 
 import express, { Request, Response } from "express";
 import cors from 'cors';
 import globalErrorHandler from "./middlewares/globalErrorhandler";
@@ -47,18 +11,24 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000','https://gardening-tips-platform-client-nine.vercel.app'];
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'http://localhost:3000',
+  'https://gardening-tips-platform-client-nine.vercel.app'
+];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Check if the incoming origin is in the allowedOrigins array
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    // Check if the incoming origin is in the allowedOrigins array or if origin is undefined (for non-browser requests)
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true,  // Allow credentials (cookies, authorization headers, etc.) if needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+  credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
 }));
 
 // application routes
@@ -76,5 +46,3 @@ app.use(globalErrorHandler);
 app.use(notFound);
 
 export default app;
-
-
