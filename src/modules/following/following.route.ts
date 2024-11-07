8a1e63745180648b8d44b5f8b4211c0797d1159c
mzajbe <mzajbe@gmail.com>
2024-11-07 15:16:30 +0600

    readme

--- src/modules/following/following.route.ts
+++ src/modules/following/following.route.ts
@@ -1,23 +1,22 @@
-
-
 import express from "express";
-import { checkFollowStatus, followUser, getFollowers, getFollowing, unfollowUser } from "./following.controller";
-
+import {
+  checkFollowStatus,
+  followUser,
+  getFollowers,
+  getFollowing,
+  unfollowUser,
+} from "./following.controller";
 
 const router = express.Router();
 
-// Follow a user
 router.post("/", followUser);
 
-// Unfollow a user
 router.delete("/unfollow", unfollowUser);
 
-// Get followers of a user
 router.get("/:userId", getFollowers);
 
-// Get users a user is following
 router.get("/following/:userId", getFollowing);
 
-router.get('/status/:followerId/:followingId', checkFollowStatus);
+router.get("/status/:followerId/:followingId", checkFollowStatus);
 
 export const FollowRoutes = router;
